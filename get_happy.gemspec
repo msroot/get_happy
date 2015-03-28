bd765150731569746d86eaa13af68435103db295
Ioannis Kolovos <yannis.kolovos@gmail.com>
2015-03-28 05:25:52 +0100

    update

--- get_happy.gemspec
+++ get_happy.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = GetHappy::VERSION
   spec.authors       = ["Ioannis Kolovos"]
   spec.email         = ["yannis.kolovos@gmail.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = "Get Happy"
+  spec.description   = "Get Happy"
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -17,7 +17,9 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
-
+  spec.executables << 'get_happy'
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_runtime_dependency "thor"
+  
 end
--- lib/get_happy.rb
+++ lib/get_happy.rb
@@ -1,5 +1,92 @@
 require "get_happy/version"
+require "thor"
+require 'yaml'
+require 'fileutils'
 
-module GetHappy
-  # Your code goes here...
+module GetHappy 
+  class CLI  < Thor
+    
+    desc "play", "play a random song"
+    def play
+      @collection ||= GetHappy.get_collection
+      system("open", @collection.sample.to_s) unless @collection.empty?
+      puts "Collection is empty! " +
+      "add some links first: get_happy seed" if @collection.empty?
+    end
+  
+  
+    desc "add", "add a link to collection"
+    def add(*links)
+      
+      @collection ||= GetHappy.get_collection
+      
+      links.each {|link| @collection << link.to_s}
+
+      GetHappy.write_collection(@collection)
+      
+      puts @collection.inspect
+    end
+    
+  
+    desc "list", "list collection"
+    def list  
+      @collection ||= GetHappy.get_collection
+      puts @collection.inspect
+    end
+    
+    
+    
+    desc "clean", "clean collection"
+    def clean
+      @collection = []
+      GetHappy.write_collection([])
+      puts @collection.inspect
+    end
+    
+    desc "seed", "seed with some sample urls"
+    def seed
+      GetHappy.seed
+      @collection = GetHappy.get_collection
+      puts @collection.inspect
+    end
+  
+  end
 end
+
+
+module GetHappy 
+  extend self
+
+  @seed = ["https://www.youtube.com/watch?v=s3Q80mk7bxE"]
+  COLLECTION_DIR = Dir.home + '/.get_happy'
+  COLLECTION_PATH = COLLECTION_DIR + "/collection.yml"
+    
+  def write_collection(collection)
+    File.open(COLLECTION_PATH, "w") do |file|
+      file.write collection.uniq.to_yaml
+    end 
+  end
+  
+  def seed
+    write_collection get_collection + @seed
+  end
+
+  def get_collection
+    unless File.directory?(COLLECTION_DIR)
+      FileUtils.mkdir_p(COLLECTION_DIR)
+    end
+
+    File.open(COLLECTION_PATH, "w+") do |file|
+      file.write @seed.to_yaml
+    end unless File.file?(COLLECTION_PATH) 
+    YAML::load_file(COLLECTION_PATH) || []  
+  end
+
+end 
+
+# rake build install && get_happy play
+# rake build install && get_happy list
+# rake build install && get_happy seed
+# rake build install && get_happy clean
+# rake build install && get_happy add https://www.youtube.com/watch?v=s3Q80mk7bxE
+# del ~/.get_happy
